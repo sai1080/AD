81512d49fb23779937d9223ec4dd8c8ec3025d73
Simon <simon@ubuntu.ubuntu-domain>
2013-02-06 10:45:15 +0100

    preparado para añadir articulo

---
+++ PNhibernate/PNhibernate/Articulo.cs
@@ -0,0 +1,14 @@
+using System;
+
+namespace Serpis.Ad
+{
+	public class Articulo
+	{
+		public virtual long Id {get; set;}
+		public virtual string Nombre {get; set;}
+		public virtual decimal Precio {get; set;}
+		//TODO property Categoria
+		
+	}
+}
+
--- PNhibernate/PNhibernate/MainWindow.cs
+++ PNhibernate/PNhibernate/MainWindow.cs
@@ -1,11 +1,8 @@
 using System;
 using Gtk;
-
 using NHibernate.Cfg;
 using NHibernate.Tool.hbm2ddl;
-
 using NHibernate;
-
 using Serpis.Ad;
 
 
@@ -22,20 +19,58 @@ public partial class MainWindow: Gtk.Window
 		configuration.AddAssembly(typeof(Categoria).Assembly);
 		
 		new SchemaExport(configuration).Execute(true, false, false);
-		//creamos la sesion
+
 		ISessionFactory sessionFactory = configuration.BuildSessionFactory();
-		//acceso a la base de datos
-		ISession session = sessionFactory.OpenSession();
-		//carge el 2L de categoria
-		Categoria categoria = (Categoria)session.Load(typeof(Categoria),2L);
-			
-		Console.WriteLine("Categoria Id={0} Nombre={1}", categoria.Id, categoria.Nombre);
-		categoria.Nombre = DateTime.Now.ToString();
-		session.SaveOrUpdate (categoria);
-		 session.Flush();                         
-		session.Close();
+
+		//modifico la categoria 2
+		//updateCategoria (sessionFactory);
+		
+		//insertCategoria(sessionFactory);
 		
 		sessionFactory.Close();
+	}
+	
+		private void loadArticulo(ISessionFactory sessionFactory){
+		using (ISession session = sessionFactory.openSessuib()){
+			Articulo articulo = session.Load(typeof(Articulo), 2L);
+			Console.WriteLine("Articulo Id={o} Nombre={1} Precio={2}",
+			              articulo.Id, articulo.Nombre, articulo.Precio);
+		}
+	}
+	
+	private void updateCategoria(ISessionFactory sessionFactory sessionFactory){
+		ISession session = sessionFactory.OpenSession();
+		try{
+			Categoria categoria = (Categoria)session.Load(typeof(Categoria),2L);			
+			Console.WriteLine("Categoria Id={0} Nombre={1}", categoria.Id, categoria.Nombre);
+			categoria.Nombre = DateTime.Now.ToString();
+			session.SaveOrUpdate (categoria);
+			session.Flush();  
+		} finally {
+			session.Close();
+		}
+	}
+	
+	/*private void insertCategoria(ISessionFactory sessionFactory){
+		ISession session = sessionFactory.OpenSession();
+		try{
+			Categoria categoria = new Categoria();
+			categoria.Nombre = "Nueva " + DateTime.Now.ToString();
+			session.SaveOrUpdate (categoria);
+			session.Flush(); 
+		} finally {
+			session.Close();
+		}
+	}*/
+	
+	//con using tenemos un idisposable y compila como si fuera un try/finally
+	private void insertCategoria(ISessionFactory sessionFactory){
+		using (ISession session = sessionFactory.OpenSession()) {		
+			Categoria categoria = new Categoria();
+			categoria.Nombre = "Nueva " + DateTime.Now.ToString();
+			session.SaveOrUpdate (categoria);
+			session.Flush(); 	
+		}
 	}
 	
 	protected void OnDeleteEvent (object sender, DeleteEventArgs a)
